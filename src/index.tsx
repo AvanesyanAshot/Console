681d980d98c855b2f4859daa1bc08278ddb68c8c
AvanesyanAshot <9teron9@gmail.com>
2022-02-23 20:47:28 +0300

    feat: add scroll style

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,20 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     font-family: consolas;
     color: white;
-
   }
+
+  *::-webkit-scrollbar {
+    width: 5px;
+  }
+  *::-webkit-scrollbar-track {
+    background: black; 
+  }
+  *::-webkit-scrollbar-thumb {
+    background-color: green;
+    border-radius: 15px;
+    border: 2px solid green;
+  }
+
 `;
 ReactDOM.render(
   <>
